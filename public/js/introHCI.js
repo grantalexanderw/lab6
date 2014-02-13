265614a78abce1bbb369210f2e8c1cc6f10a8d67
vagrant <vagrant@precise32.(none)>
2014-02-13 18:54:32 +0000

    AJAX LABBB

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,13 +26,30 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	var detailz = $(this).next();
+
+			$.get("/project/" + idNumber, function(result){
+				detailz.html(result.summary);
+			});
+
+
 	console.log("User clicked on project " + idNumber);
 }
+
+//function addProject(result){//
+//	console.log(result);
+//}
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
+
+	var woop = $(this).next();
+
+	$.get("/palette/", function(result){
+		woop.html(result.summary);
+	});
 	console.log("User clicked on color button");
 }
